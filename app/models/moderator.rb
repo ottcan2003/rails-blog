a0143fe911e6ee01f18c077d42e05b7d8bda0465
Bev Yu <temp1298temp@yandex.com>
2016-12-30 18:36:52 -0500

    test

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -2,4 +2,9 @@ class Moderator < ApplicationRecord
 	has_many :posts
 	
 	has_secure_password
+
+	validates :fullname, presence: true
+	validates :username, presence: true, format: {with: /@/, message: 'is not valid'}
+	validates :password, presence: true
+
 end
