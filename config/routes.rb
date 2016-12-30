9d228336fa7252d1653c4ebebe3cfb4d83f89ec8
Bev Yu <temp1298temp@yandex.com>
2016-12-30 15:40:38 -0500

    get moderator index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'admin/moderators#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do 
   	resources :moderators, only:[:index, :edit, :update]
