5d869043a6d32bbf9da572afce34b7216aad1e29
Bev Yu <temp1298temp@yandex.com>
2016-12-30 17:45:13 -0500

    no comment

--- app/controllers/admin/moderators_controller.rb
+++ app/controllers/admin/moderators_controller.rb
@@ -2,4 +2,14 @@ class Admin::ModeratorsController < Admin::ApplicationController
 	def index
 		@moderators = Moderator.all
 	end
+
+	def edit
+		@moderator = Moderator.find(params[:id])
+	end
+
+	private
+
+	def moderator_params
+		params.require(:moderator).permit(:id, :fullname, :username, :password)
+	end	
 end
